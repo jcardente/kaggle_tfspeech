319797f6416ffa22f0e4c730ddaf09a0e03f6c65
John Cardente <jcardente@gmail.com>
2017-12-19 20:16:28 -0500

    Added validation code

--- src/train.py
+++ src/train.py
@@ -2,19 +2,21 @@
 # train.py
 #
 # Train a RNN solution for the Kaggle TF Speech challenge
-
+import argparse
 import util
 import tensorflow as tf
 import numpy as np
+import time
 
 FLAGS = None
 
 # DEFAULT PARAMETERS
 framesPerWindow      = 512
 overlapRate          = 4
-validationPercentage = 20
+validationPercentage = 5
 numEpochs             = 1
 learningRate          = 0.001
+batchSize             = 32
 
 def makeInputGenerator(dataset, framesPerWindow, overlapRate):
     def gen():
@@ -47,35 +49,56 @@ if __name__ == '__main__':
     
     # build input pipeline using a generator
     tf.reset_default_graph()
-    train_gen     = makeInputGenerator(datasets['training'], framesPerWindow, overlapRate)
-    train_data    = tf.data.Dataset.from_generator(train_gen,
-                                                   (tf.int32, tf.float32),
-                                                   ([],[nsteps,ninputs]))
-    train_data    = train_data.shuffle(buffer_size=1000)
-    train_data    = train_data.batch(4)
-    #train_data    = train_data.repeat(1)
-    #iterator = train_data.make_one_shot_iterator()
-    iterator = train_data.make_initializable_iterator()
-    batch_labels, batch_data = iterator.get_next()
 
+    with tf.device("/cpu:0"):
+        # Training data set
+        train_gen     = makeInputGenerator(datasets['training'], framesPerWindow, overlapRate)
+        train_data    = tf.data.Dataset.from_generator(train_gen,
+                                                       (tf.int32, tf.float32),
+                                                       ([],[nsteps,ninputs]))
+        #train_data    = train_data.shuffle(buffer_size=1000)
+        train_data    = train_data.batch(batchSize)
+
+
+        # Validation data set
+        val_gen     = makeInputGenerator(datasets['validation'], framesPerWindow, overlapRate)
+        val_data    = tf.data.Dataset.from_generator(val_gen,
+                                                       (tf.int32, tf.float32),
+                                                       ([],[nsteps,ninputs]))
+        val_data    = val_data.batch(batchSize)
+
+        
+        # Reinitializable iterator
+        #iterator = train_data.make_initializable_iterator()
+        iterator = tf.data.Iterator.from_structure(train_data.output_types, train_data.output_shapes)
+        batch_labels, batch_data = iterator.get_next()
+
+        train_init_op = iterator.make_initializer(train_data)
+        val_init_op   = iterator.make_initializer(val_data)
+
+        
     # Build the model
-    basic_cell = tf.contrib.rnn.BasicRNNCell(num_units=50)
-    outputs, states = tf.nn.dynamic_rnn(basic_cell, batch_data, dtype=tf.float32)
-    logits = tf.layers.dense(states, noutputs)
-    xentropy = tf.nn.sparse_softmax_cross_entropy_with_logits(labels=batch_labels, logits=logits)
-    loss = tf.reduce_mean(xentropy)
-    optimizer = tf.train.AdamOptimizer(learning_rate = learningRate)
-    training_op = optimizer.minimize(loss)
-    correct = tf.nn.in_top_k(logits, batch_labels, 1)
-    accuracy = tf.reduce_mean(tf.cast(correct, tf.float32))
+    with tf.device("/gpu:0"):
+        basic_cell = tf.contrib.rnn.BasicRNNCell(num_units=100)
+        outputs, states = tf.nn.dynamic_rnn(basic_cell, batch_data, dtype=tf.float32)
+        logits = tf.layers.dense(states, noutputs)
+        xentropy = tf.nn.sparse_softmax_cross_entropy_with_logits(labels=batch_labels, logits=logits)
+        loss = tf.reduce_mean(xentropy)
+        optimizer = tf.train.AdamOptimizer(learning_rate = learningRate)
+        training_op = optimizer.minimize(loss)
+
+    with tf.device("/cpu:0"):
+        correct = tf.nn.in_top_k(logits, batch_labels, 1)
+        accuracy = tf.reduce_mean(tf.cast(correct, tf.float32))
 
     # Start the training loop
+    saver = tf.train.Saver()    
     init_op = tf.global_variables_initializer()
     losses  = []
     batch_count = 0
     with tf.Session() as sess:
         sess.run(init_op)
-        sess.run(iterator.initializer)
+        sess.run(train_init_op)
         for epoch in range(numEpochs):
             print("Epoch " + str(epoch))
             while True:
@@ -83,13 +106,35 @@ if __name__ == '__main__':
                     _ , batch_loss = sess.run([training_op, loss])
                     losses.append(batch_loss)
                     if batch_count % 100 == 0:
-                        print("Batch {} loss {}".format(batch_count, batch_loss))
+                        batch_accuracy = sess.run(accuracy)
+                        print("Batch {} loss {} accuracy {}".format(batch_count, batch_loss, batch_accuracy))
                     batch_count += 1
                 except tf.errors.OutOfRangeError:
                     break
+        ckptName = './tmp/model_' + time.strftime('%Y%m%d_%H%M%S') + '.ckpt'
+        print("Saving parameters to file {}".format(ckptName))
+        save_path = saver.save(sess, ckptName)
 
+                
+        # Now do validation
+        print("Starting validation....")
+        sess.run(val_init_op)
+        numCorrect = 0
+        numTotal   = 0
+        batch_count = 0
+        while True:
+            try:
+                batch_correct, batch_accuracy = sess.run([correct, accuracy])
+                numCorrect += np.sum(batch_correct)
+                numTotal   += len(batch_correct)
+                if batch_count % 100 == 0:
+                    print("Batch {} accuracy {}".format(batch_count, batch_accuracy))
+                batch_count += 1
+            except tf.errors.OutOfRangeError:
+                break
 
-        
+    print("Validation Correct: {}  Total: {}".format(numCorrect, numTotal))
+
 
 
 

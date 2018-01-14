882b0190d53936212181f93dca402040c7822ab3
John Cardente <jcardente@gmail.com>
2018-01-14 15:09:16 -0500

    New model

--- src/models.py
+++ src/models.py
@@ -115,23 +115,26 @@ def conv2DRnn(batch_data, noutputs, nhidden, isTraining):
 
     data  = tf.expand_dims(batch_data, -1)
     
-    conv1 = tf.layers.conv2d(data, filters=32, kernel_size=[3,5], strides=[1,1],
-                             padding='VALID', activation=None)
+    conv1 = tf.layers.conv2d(data, filters=64, kernel_size=[10,5], strides=[1,2],
+                             padding='SAME', activation=None)
     conv1 = tf.layers.batch_normalization(conv1, training=isTraining, momentum=0.9)
     conv1 = tf.nn.relu(conv1)
-    conv1 = tf.layers.dropout(conv1, rate=0.2, training=isTraining)
+    conv1 = tf.nn.max_pool(conv1, ksize=[1,3,5,1], strides=[1,1,2,1], padding="SAME")
+    conv1 = tf.layers.dropout(conv1, rate=0.5, training=isTraining)
     
-    conv2 = tf.layers.conv2d(conv1, filters=16, kernel_size=[3,5], strides=[1,1],
-                             padding='VALID', activation=None)
+    conv2 = tf.layers.conv2d(conv1, filters=128, kernel_size=[10,5], strides=[1,2],
+                             padding='SAME', activation=None)
     conv2 = tf.layers.batch_normalization(conv2, training=isTraining, momentum=0.9)
     conv2 = tf.nn.relu(conv2)
-    conv2 = tf.layers.dropout(conv2, rate=0.2, training=isTraining)    
+    conv2 = tf.nn.max_pool(conv2, ksize=[1,3,3,1], strides=[1,1,1,1], padding="SAME")    
+    conv2 = tf.layers.dropout(conv2, rate=0.5, training=isTraining)    
     
-    conv3 = tf.layers.conv2d(conv2, filters=8, kernel_size=[3,5], strides=[1,1],
-                             padding='VALID', activation=None)
+    conv3 = tf.layers.conv2d(conv2, filters=256, kernel_size=[10,5], strides=[1,1],
+                             padding='SAME', activation=None)
     conv3 = tf.layers.batch_normalization(conv3, training=isTraining, momentum=0.9)
     conv3 = tf.nn.relu(conv3)
-    conv3 = tf.layers.dropout(conv3, rate=0.2, training=isTraining)
+    conv3 = tf.nn.max_pool(conv3, ksize=[1,3,5,1], strides=[1,1,1,1], padding="VALID")        
+    conv3 = tf.layers.dropout(conv3, rate=0.5, training=isTraining)
     
     conv3shape = conv3.shape.as_list()
     squeezed = tf.reshape(conv3, (-1, conv3shape[1], conv3shape[2]*conv3shape[3]))
@@ -140,7 +143,7 @@ def conv2DRnn(batch_data, noutputs, nhidden, isTraining):
 
     basic_cell = tf.contrib.rnn.GRUBlockCellV2(num_units=nhidden)
     output_seqs, states = tf.contrib.rnn.static_rnn(basic_cell, X_seqs, dtype=tf.float32)
-    fc1   =  tf.layers.dense(states, nhidden)
-    logits = tf.layers.dense(fc1, noutputs)
+
+    logits = tf.layers.dense(states, noutputs)
     
     return logits
